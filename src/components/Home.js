df66805c83a3746d5a572efbf11d4337f38153fc
Mark Muchai <muchaimark6@gmail.com>
2023-07-23 18:03:17 +0300

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,18 @@ import React from "react";
 
 function Home({ color, username, city }) {
   return (
+    <>
     <div id="home">
       <h1 style={{ color: color }}>
         {username} is a Web Developer from {city}
       </h1>
     </div>
+    <div id="about">
+      <h1>
+       This is my portfolio
+      </h1>
+    </div>
+    </>
   );
 }
 
